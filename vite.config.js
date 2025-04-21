0a0b140f620f22b8d3ba8efcc12e587898447c43
zhafran12382 <90399680+zhafran12382@users.noreply.github.com>
2025-04-21 08:36:54 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Konfigurasi Vite untuk proyek React
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000,
+    port: 8000,  // Menentukan port untuk server lokal saat pengembangan
   },
   build: {
-    outDir: 'dist',
+    outDir: 'dist',  // Menentukan direktori output build
   },
 });
